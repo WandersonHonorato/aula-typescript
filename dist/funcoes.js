85b0f77e69aeeb6e98c7da6862076e4393dd00f8
Wanderson Honorato <wanderson.honorato1998@gmail.com>
2025-04-30 00:33:48 -0300

    Primeiro commit

---
+++ dist/funcoes.js
@@ -0,0 +1,3 @@
+"use strict";
+const saudacao = "Olá, mundo!";
+console.log(saudacao);
---
+++ src/funcoes.ts
@@ -0,0 +1,7 @@
+function somar_ts(x: number, y: number): number {
+    return x + y;
+  }
+  let a: number = 10; 
+  let b: number = 20; 
+  let soma_ts = somar_ts(a, b);
+  console.log(`A soma de ${a} com ${b} é: ${soma_ts}`);
